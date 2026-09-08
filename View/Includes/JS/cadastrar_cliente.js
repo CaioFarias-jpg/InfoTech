9009dcc4ada22d64b21aa9f48adfb4b3143341fa
Caio Farias <caiovv1@outlook.com>
2026-09-08 10:01:47 -0400

    update

--- View/Includes/JS/cadastrar_cliente.js
+++ View/Includes/JS/cadastrar_cliente.js
@@ -1,6 +1,17 @@
 let nome = document.getElementById('nome');
 
-nome.addEventListener('change', function(event){
+nome.addEventListener('change', async function(event){
     event.preventDefault();
-    alert(nome.value);
+    
+    try {
+        const response = await fetch('/infotech/API/action_cadastro_cliente.php');
+        const result = await response.json();
+
+        if(result.status == 200){
+            alert("Deu certo a comunicação com a API via PHP");
+        }
+    }
+    catch(error){
+        alert('ERROOOOOOO');
+    }
 });
