879f7128e8cc1f193fd226fffaa28a5b018599a4
Caio Farias <caiovv1@outlook.com>
2026-09-17 08:18:41 -0400

    update

--- View/Includes/js/cadastro_cliente.js
+++ View/Includes/js/cadastro_cliente.js
@@ -11,7 +11,7 @@ formCliente.addEventListener('submit', async (event) => {
             method: 'POST',
             body: new FormData(formCliente) // já inclui o id_categoria do select
         });
-        const result = await response.json();//att
+        const result = await response.json();
 
         if (result.status === 200) {
             alert("Cadastrado com sucesso!!");
@@ -46,3 +46,8 @@ document.addEventListener("DOMContentLoaded", async function () {
 // modalCliente.addEventListener('close', () => {
 //     window.location.href = '/infotech/cliente/listar';
 // });
+
+
+
+//GET = SOLICITA UMA REQUISIÇÃO AO SERVER EM UMA ROTA ESPECIFICA
+//POST = ENVIA DADOS AO SERVER
